bb07cf8a9a9a486036e2cb728855efc3387c1647
Manoel Carvalho <manoelmecat@gmail.com>
2020-01-21 17:09:23 -0300

    tags

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './global.css'
+import './sidebar.css'
+import './main.css'
+
+import TagsInput from 'react-tagsinput'
+import 'react-tagsinput/react-tagsinput.css'
 
 function App() {
-  return (
+
+  const [tags, setTags] = useState([])
+
+  function handleAddTags(tags) {
+    setTags(tags)
+  }
+
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside>
+        <strong>Register</strong>
+        <form>
+
+          <div className="input-block">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="Github Username" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Techs</label>
+            <TagsInput value={tags} onChange={handleAddTags} name="techs" id="techs"/>
+          </div>
+
+          <div className="input-group">
+            
+            <div class="input-block">
+              <label htmlFor="long">Longitude</label>
+              <input type="text" name="long" id="long" placeholder="0.000000"/>
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="lat">Latitude</label>
+              <input type="text" name="lat" id="lat" placeholder="0.000000"/>
+            </div>
+
+          </div>
+          <button type="submit">Save</button>
+        </form>
+      </aside>
+      <main></main>
     </div>
   );
 }
 
-export default App;
+export default App
